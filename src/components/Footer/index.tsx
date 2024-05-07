0d384628e463c46bd296374f5e1eadb9bd005b12
gaeun <kge0211114@gmail.com>
2024-05-07 21:36:35 +0900

    HotFix: 중간 정검

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,3 +1,9 @@
 export default function Footer() {
-  return <footer />;
+  return (
+    <footer>
+      <a href="https://www.flaticon.com/kr/free-icons/-" title="페이스 북 좋아요 아이콘">
+        페이스 북 좋아요 아이콘 제작자: Ranah Pixel Studio - Flaticon
+      </a>
+    </footer>
+  );
 }
