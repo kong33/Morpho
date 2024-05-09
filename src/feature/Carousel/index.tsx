2790c538a4ad6d6668a4f0e448326a2cd3aaa29d
gaeun <kge0211114@gmail.com>
2024-05-10 00:31:09 +0900

    Feat: carousel제작

---
+++ src/feature/Carousel/index.tsx
@@ -0,0 +1,14 @@
+// import React, { ReactElement } from 'react';
+// import useEmblaCarousel from 'embla-carousel-react/components/useEmblaCarousel';
+// import styles from '@/feature/Carousel/index.module.scss';
+// export default function Carousel({ carouselItem }: { carouselItem: ReactElement[] }) {
+//   const [emblaRef] = useEmblaCarousel();
+
+//   return (
+//     <div className={styles.embla} ref={emblaRef}>
+//       {carouselItem.map((item) => (
+//         <div className={styles.embla__slide}>{item}</div>
+//       ))}
+//     </div>
+//   );
+// }
