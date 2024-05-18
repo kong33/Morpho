8a7093a3de361aa49ae2cb9deec7a4555debe6c1
gaeun <kge0211114@gmail.com>
2024-05-18 19:14:16 +0900

    Feat: carousel 제작

--- src/feature/Carousel/CarouselDotButton/index.tsx
+++ src/feature/Carousel/CarouselDotButton/index.tsx
@@ -1,59 +1,58 @@
-// import React, { PropsWithChildren, useCallback, useEffect, useState } from 'react';
-// import { EmblaCarouselType } from 'embla-carousel';
+/* eslint-disable */
+import React, { PropsWithChildren, useCallback, useEffect, useState } from 'react';
+import { EmblaCarouselType } from 'embla-carousel';
+import '../index.module.scss';
+type UseDotButtonType = {
+  selectedIndex: number;
+  scrollSnaps: number[];
+  onDotButtonClick: (index: number) => void;
+};
 
-// type UseDotButtonType = {
-//   selectedIndex: number;
-//   scrollSnaps: number[];
-//   onDotButtonClick: (index: number) => void;
-// };
+export const useDotButton = (emblaApi: EmblaCarouselType | undefined): UseDotButtonType => {
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const [scrollSnaps, setScrollSnaps] = useState<number[]>([]);
 
-// export const useDotButton = (emblaApi: EmblaCarouselType | undefined): UseDotButtonType => {
-//   const [selectedIndex, setSelectedIndex] = useState(0);
-//   const [scrollSnaps, setScrollSnaps] = useState<number[]>([]);
+  const onDotButtonClick = useCallback(
+    (index: number) => {
+      if (!emblaApi) return;
+      emblaApi.scrollTo(index);
+    },
+    [emblaApi]
+  );
 
-//   const onDotButtonClick = useCallback(
-//     (index: number) => {
-//       if (!emblaApi) return;
-//       emblaApi.scrollTo(index);
-//     },
-//     [emblaApi]
-//   );
+  const onInit = useCallback((emblaApi: EmblaCarouselType) => {
+    setScrollSnaps(emblaApi.scrollSnapList());
+  }, []);
 
-//   const onInit = useCallback((emblaApi: EmblaCarouselType) => {
-//     setScrollSnaps(emblaApi.scrollSnapList());
-//   }, []);
+  const onSelect = useCallback((emblaApi: EmblaCarouselType) => {
+    setSelectedIndex(emblaApi.selectedScrollSnap());
+  }, []);
 
-//   const onSelect = useCallback((emblaApi: EmblaCarouselType) => {
-//     setSelectedIndex(emblaApi.selectedScrollSnap());
-//   }, []);
+  useEffect(() => {
+    if (!emblaApi) return;
 
-//   useEffect(() => {
-//     if (!emblaApi) return;
+    onInit(emblaApi);
+    onSelect(emblaApi);
+    emblaApi.on('reInit', onInit).on('reInit', onSelect).on('select', onSelect);
+  }, [emblaApi, onInit, onSelect]);
 
-//     onInit(emblaApi);
-//     onSelect(emblaApi);
-//     emblaApi.on('reInit', onInit);
-//     emblaApi.on('reInit', onSelect);
-//     emblaApi.on('select', onSelect);
-//   }, [emblaApi, onInit, onSelect]);
+  return {
+    selectedIndex,
+    scrollSnaps,
+    onDotButtonClick
+  };
+};
 
-//   return {
-//     selectedIndex,
-//     scrollSnaps,
-//     onDotButtonClick
-//   };
-// };
+type PropType = PropsWithChildren<
+  React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>
+>;
 
-// type PropType = PropsWithChildren<
-//   React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>
-// >;
+export const DotButton = (props: PropType) => {
+  const { children, ...restProps } = props;
 
-// export const DotButton: React.FC<PropType> = (props) => {
-//   const { children, ...restProps } = props;
-
-//   return (
-//     <button type="button" {...restProps}>
-//       {children}
-//     </button>
-//   );
-// };
+  return (
+    <button type="button" {...restProps}>
+      {children}
+    </button>
+  );
+};
--- src/feature/HomeContent/index.tsx
+++ src/feature/HomeContent/index.tsx
@@ -1,13 +1,21 @@
+import { EmblaOptionsType } from 'embla-carousel';
+
 import Card from '@/components/Card';
-import Hero from '@/components/Hero';
+import EmblaCarousel from '@/feature/Carousel/index';
 import styles from '@/feature/HomeContent/index.module.scss';
 import { productData } from '@/libs/constants/homepage';
-import imageUrl1 from '@/public/images/homeHeroi.png';
+import banner1 from '@/public/images/homeBanner/banner1.jpg';
+import banner2 from '@/public/images/homeBanner/banner2.jpg';
+import banner3 from '@/public/images/homeBanner/banner3.jpg';
+import banner4 from '@/public/images/homeBanner/banner4.jpg';
+
+const OPTIONS: EmblaOptionsType = { loop: true };
+const SLIDES = [banner1, banner2, banner3, banner4];
 
 export default function HomeContent() {
   return (
     <>
-      <Hero type="halfImage" imageUrl={imageUrl1} title="" description="" />
+      <EmblaCarousel slides={SLIDES} options={OPTIONS} />
       <div className={styles.cardWrapper}>
         {productData.map((data) => {
           const { type, title, description, imageUrl, redirectUrl, isHoverAble, className } = data;
